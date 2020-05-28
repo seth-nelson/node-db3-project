f8f200f6be0077d6d36f249f660e14800ed9ec9d
Seth Nelson <seth.nelson@outlook.com>
2020-05-27 20:06:23 -0700

    added more queries to SQL querry file

--- queries.sql
+++ queries.sql
@@ -8,7 +8,7 @@ ON Product.CategoryId = Category.id
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
 
-SELECT [Order].id, Shipper.CompanyName from [Order]
+SELECT [Order].id, Shipper.CompanyName FROM [Order]
 Join Shipper
 ON [Order].ShipVia = Shipper.id
 WHERE [Order].OrderDate < '2012-08-09'
@@ -16,5 +16,12 @@ WHERE [Order].OrderDate < '2012-08-09'
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
 
+SELECT Product.ProductName, OrderDetail.OrderId FROM Product
+JOIN OrderDetail
+ON OrderDetail.ProductId = Product.id
+WHERE OrderDetail.OrderId = '10251'
+ORDER BY Product.ProductName
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+
+SELECT 
